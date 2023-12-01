6d94dfea16655c8c6dbfc17f564ad5d22f195ea6
prashantpatil2048 <mailto:prashantp2048@gmail.com>
2023-12-01 14:01:49 +0530

    jklk

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react';
 import MyTable from './MyTable'
 import PieChartWithPaddingAngle  from './Insights'
+import InTable from './InTable';
 
 const Dashboard = () => {
+  const [showChart, setShowChart] = useState(true);
   return (
     <div className="fixed top-15 left-0 w-full h-full bg-slate-100 bg-opacity-50 flex items-center justify-center">
       {/* Your content goes here */}
@@ -15,7 +17,20 @@ const Dashboard = () => {
           </div>
           <div className="bg-white bg-opacity-400 px-16 py-32 rounded-lg">
           <div className=" bg-white bg-opacity-80 h-full w-full rounded-sm mt-10 mx-4 ">
-          <PieChartWithPaddingAngle />
+          
+          <div className="container mx-auto mt-8">
+      <div className="flex items-center mb-4">
+        <label className="mr-2">Show Doughnut Chart:</label>
+        <input
+          type="checkbox"
+          checked={showChart}
+          onChange={() => setShowChart(!showChart)}
+          className="form-checkbox h-5 w-5 text-blue-500"
+        />
+      </div>
+
+      {showChart ?<PieChartWithPaddingAngle /> : <InTable/>}
+    </div>
           </div>
           </div>
         </div>
---
+++ src/component/InTable.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+const InTable = () => {
+  const data = ([
+    { id: 1,  Person: 'Male', Persentage: 40},
+    { id: 2,  Person: 'Female', Persentage:25},
+    { id: 3,  Person: 'Unknown', Persentage: 35},
+    
+    // Add more data as needed
+  ]);
+
+
+
+ 
+
+  return (
+    <div className="max-w-md mx-auto my-10">
+      <div className="shadow border-b border-gray-200 sm:rounded-lg">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+          
+            <tr>
+              <th
+                scope="col"
+                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer"
+                
+              >
+             id
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer"
+             
+              >
+             Person
+              </th>
+              <th
+                scope="col"
+                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider cursor-pointer"
+               
+              >
+             Persentage
+              </th>
+            
+            </tr>
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200">
+         
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td className="px-6 py-4 whitespace-nowrap">{item.id}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{item.Person}</td>
+                <td className="px-6 py-4 whitespace-nowrap">{item. Persentage}%</td>
+              </tr>
+            ))}
+           
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default InTable;
