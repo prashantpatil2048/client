da74d3ce6a7f658642fbd008d9b9594b1cd25c07
prashantpatil2048 <mailto:prashantp2048@gmail.com>
2023-12-02 09:58:43 +0530

    commited

--- src/component/Createads.js
+++ src/component/Createads.js
@@ -1,9 +1,19 @@
-import { useState } from "react"
+import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Createads = () => {
-    const [isChecked, setIsChecked] = useState(false);
-    const [isCheckedm, setIsCheckedm] = useState(false);
+  const [isChecked, setIsChecked] = useState(false);
+  const [isCheckedm, setIsCheckedm] = useState(false);
+  const navigate = useNavigate();
+  function addfrom(e) {
+    e.preventDefault();
 
+
+
+    navigate('/');
+
+
+  }
 
   const handleCheckboxChange = () => {
     setIsChecked(!isChecked);
@@ -11,66 +21,66 @@ const Createads = () => {
   const handleCheckboxChangem = () => {
     setIsCheckedm(!isCheckedm);
   };
-    return (
-        <div className="fixed top-15 left-0 w-full h-full bg-slate-100 bg-opacity-50 flex items-center justify-center">
-            {/* Your content goes here */}
-            <div className=" bg-white bg-opacity-80 h-full w-full rounded-sm mt-10 mx-4 ">
-                <h4 className="text-xl  mt-2 ml-2">Create Ads</h4>
-                {/* Add your modal content */}
-                <div className='flex items-center justify-center'>
-                    <div className="bg-white shadow-md bg-opacity-400 px-16 py-32 m-4 rounded-lg">
-                    <input
-                    type="checkbox"
-                    id="myCheckbox"
-                    checked={isChecked}
-                    onChange={handleCheckboxChange}
-                    className="form-checkbox h-5 w-5 text-blue-500 focus:ring-blue-400 border-gray-300 rounded"
-                  />
-                  {isChecked ? (
-                    <div className="mb-4">
-                      <label className="block text-sm font-medium text-gray-700">Text Input</label>
-                      <input
-                                      type="text"
-                                      className="mt-1 p-2 border rounded-md w-full"
-                                      placeholder="Enter text"
-                                    />
-                    </div>
-                  ): <p className="text-slate-300 flex justify-center">Create</p>}
-                 
-                        <h2 className="text-2xl font-bold mb-4 flex justify-center">Text Ad</h2>
-                        
-                    </div>
-                    <div className="bg-white shadow-md bg-opacity-400 px-16 py-32 m-4 rounded-lg">
-                    <input
-                    type="checkbox"
-                    id="myCheckbox"
-                    checked={isCheckedm}
-                    onChange={handleCheckboxChangem}
-                    className="form-checkbox h-5 w-5 text-blue-500 focus:ring-blue-400 border-gray-300 rounded"
-                  />
-                  {isCheckedm ? (
-                    <div className="mb-4">
-                      <label className="block text-sm font-medium text-gray-700">Media Input</label>
-                      <input
-                        type="file"
-                        className="mt-1 p-2 border rounded-md w-full"
-                        accept="image/*, video/*"
-                      />
-                    </div>
-                  ): <p className="text-slate-300 flex justify-center">Create</p>}
-                 
-                        <h2 className="text-2xl font-bold flex justify-center mb-4">Media Ad</h2>
-                        
-                    </div>
-                </div>
-                <div className='flex justify-end mr-4'>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-12 rounded">
-                Next
-              </button>
+  return (
+    <div className="fixed top-15 left-0 w-full h-full bg-slate-100 bg-opacity-50 flex items-center justify-center">
+      {/* Your content goes here */}
+      <div className=" bg-white bg-opacity-80 h-full w-full rounded-sm mt-10 mx-4 ">
+        <h4 className="text-xl  mt-2 ml-2">Create Ads</h4>
+        {/* Add your modal content */}
+        <div className='flex items-center justify-center'>
+          <div className="bg-white shadow-md bg-opacity-400 px-16 py-32 m-4 rounded-lg">
+            <input
+              type="checkbox"
+              id="myCheckbox"
+              checked={isChecked}
+              onChange={handleCheckboxChange}
+              className="form-checkbox h-5 w-5 text-blue-500 focus:ring-blue-400 border-gray-300 rounded"
+            />
+            {isChecked ? (
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">Text Input</label>
+                <input
+                  type="text"
+                  className="mt-1 p-2 border rounded-md w-full"
+                  placeholder="Enter text"
+                />
               </div>
-            </div>
+            ) : <p className="text-slate-300 flex justify-center">Create</p>}
+
+            <h2 className="text-2xl font-bold mb-4 flex justify-center">Text Ad</h2>
+
+          </div>
+          <div className="bg-white shadow-md bg-opacity-400 px-16 py-32 m-4 rounded-lg">
+            <input
+              type="checkbox"
+              id="myCheckbox"
+              checked={isCheckedm}
+              onChange={handleCheckboxChangem}
+              className="form-checkbox h-5 w-5 text-blue-500 focus:ring-blue-400 border-gray-300 rounded"
+            />
+            {isCheckedm ? (
+              <div className="mb-4">
+                <label className="block text-sm font-medium text-gray-700">Media Input</label>
+                <input
+                  type="file"
+                  className="mt-1 p-2 border rounded-md w-full"
+                  accept="image/*, video/*"
+                />
+              </div>
+            ) : <p className="text-slate-300 flex justify-center">Create</p>}
+
+            <h2 className="text-2xl font-bold flex justify-center mb-4">Media Ad</h2>
+
+          </div>
         </div>
-    )
+        <div className='flex justify-end mr-4'>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-12 rounded" onClick={addfrom}>
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Createads
--- src/component/Insights.js
+++ src/component/Insights.js
@@ -2,38 +2,40 @@ import React from 'react';
 import { PieChart, Pie, Legend, Tooltip, Cell } from 'recharts';
 
 const chartData = [
-  { name: '40% Male', value: 40  },
+  { name: '40% Male', value: 40 },
   { name: '35% Female', value: 35 },
-  { name: '25% Unknown', value: 25  },
+  { name: '25% Unknown', value: 25 },
 ];
 
 const COLORS = ['#ff7843', 'blue', 'black'];
 
 const PieChartWithPaddingAngle = () => {
   return (
-    <div className="max-w-md mx-auto">
-    <div className="border-2 border-slate-200">
-    <PieChart width={400} height={400}>
-      <Pie
-        data={chartData}
-        cx={200}
-        cy={200}
-        innerRadius={60}
-        outerRadius={80}
-        fill="#8884d8"
-        paddingAngle={5} // Adjust the padding angle as needed
-        dataKey="value"
-      >
-        {chartData.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-      <Tooltip />
-      <Legend />
-    </PieChart>
-    </div></div>
+    <div className="max-w-md mx-auto flex">
+      <div className="border-2 border-slate-200">
+        <PieChart width={400} height={400}>
+          <Pie
+            data={chartData}
+            cx={200}
+            cy={200}
+            innerRadius={60}
+            outerRadius={80}
+            fill="#8884d8"
+            paddingAngle={5} // Adjust the padding angle as needed
+            dataKey="value"
+          >
+            {chartData.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+            ))}
+          </Pie>
+          <Tooltip />
+          <Legend />
+        </PieChart>
+      </div>
+
+      {/* Render the Cell component here if needed */}
+    </div>
   );
 };
 
 export default PieChartWithPaddingAngle;
-
